6167031c47a5391d0e565bfc1b2daea391de84f6
Zanaku <xzanaku@gmail.com>
2020-12-01 09:12:21 +0000

    removed extra junk

--- src/main/java/com/appsecco/dvja/controllers/PingAction.java
+++ src/main/java/com/appsecco/dvja/controllers/PingAction.java
@@ -39,38 +39,9 @@ public class PingAction extends BaseController {
 
         return SUCCESS;
     }
-    //TODO: DELETE ME
-    private String escape(String input){
-        return input.replaceAll("[^A-Za-z0-9 ]+","");
-    }
-    //TODO: DELETE ME
-    private void doExecCommandSafe() throws IOException{
-        Runtime runtime = Runtime.getRuntime(); //String[] command = { "/bin/bash", "-c", "ping -t 5 -c 5 " + getAddress() };
-        String[] command ={             "cmd.exe",
-                "/c echo This is a very safe operation From: " + this.escape(getAddress())
-        };
-        Process process = runtime.exec(command);
-
-        BufferedReader  stdinputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = null;
-        String output = "Output:\n\n";
-
-        while((line = stdinputReader.readLine()) != null)
-            output += line + "\n";
-
-        output += "\n";
-        output += "Error:\n\n";
-
-        stdinputReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-        while((line = stdinputReader.readLine()) != null)
-            output += line + "\n";
-
-        setCommandOutput(output);
-    }
 
     private void doExecCommand() throws IOException {
         Runtime runtime = Runtime.getRuntime();
-        //String[] command = { "/bin/bash", "-c", "ping -t 5 -c 5 " + getAddress() };
         String[] command ={
                 "cmd.exe",
                 "/c echo This is a very safe operation From: " + getAddress()
